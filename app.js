4728ec4e031ea96cf34b0a34103177f57cc884cd
Jayson <oliciajayson@gmail.com>
2023-08-01 08:57:48 +0800

    testimonial listing and download functionality

--- app.js
+++ app.js
@@ -17,10 +17,9 @@ navLinks.forEach((link) => {
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   slidesPerView: 1,
-  loop: true,
   direction: "horizontal",
   autoplay: {
-    delay: 5000,
+    delay: 3000,
   },
   // If we need pagination
   pagination: {
@@ -47,3 +46,36 @@ const swiper = new Swiper(".swiper", {
 });
 
 //
+
+// Data Fetching
+async function loadTestimonials() {
+  const data = await fetch("./data/testimonials.json");
+  const result = await data.json();
+  result.testimonials.forEach((testimonial) => {
+    displayTestimonials(testimonial);
+  });
+}
+
+function displayTestimonials(result) {
+  const swiperContainer = document.querySelector(".swiper-wrapper");
+  const slideContainer = document.createElement("div");
+  slideContainer.classList.add("swiper-slide");
+  const { href, text, credit } = result;
+  slideContainer.innerHTML = `
+              <div class="slide-container">
+                <div class="image-container">
+                  <img
+                    src="${href}"
+                    class="customer-image"
+                    alt="${credit}"
+                  />
+                </div>
+                <blockquote class="blockqoute">
+                  <p class="blockqoute-text">${text}</p>
+                  <h3 class="customer-name">${credit}</h3>
+                </blockquote>
+              </div>
+  `;
+  swiperContainer.append(slideContainer);
+}
+loadTestimonials();
